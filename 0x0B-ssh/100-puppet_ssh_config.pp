cb0bed197a8e28fb0101d2f978f865bd97025ce3
Waliu Ogunlaru <waliu.ogunlaru@yahoo.com>
2022-12-23 08:01:23 +0100

    task_adv

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,14 @@
+# make changes to our configuration file
 
+file_line {'password-less':
+path    => '/etc/ssh/ssh_config',
+line    => '    PasswordAuthentication no',
+replace => true
+}
+
+
+file_line {'private-key':
+path    => '/etc/ssh/ssh_config',
+line    => '    IdentityFile ~/.ssh/school',
+replace => true
+}
